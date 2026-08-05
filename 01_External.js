03eed4827fa1efa48b8b5023cf6e94c3ff26a7a9
Rahul S <rahulsan098@gmail.com>
2026-08-05 11:42:57 +0530

    code

--- 01_External.js
+++ 01_External.js
@@ -1,3 +1,6 @@
+/*External Js
+Js Code is wrriten in a separate.js File
+To Link we use script tag along with src attribute*/
 console.log("External JS 😍")
 console.log("External JS 😍")
 console.log("External JS 😍")
---
+++ 02_Variables.js
@@ -0,0 +1,11 @@
+//! var (Old Way)
+var a = 10;
+console.log(a); //10
+
+//! let (Modern and recommended)
+let b = 20;
+console.log(b); //20
+
+//! const (constant value)
+const c = 30;
+console.log(c); //30
