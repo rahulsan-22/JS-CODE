7e1ed66097436ffedcf34df9e11fa0082f0c419f
Rahul S <rahulsan098@gmail.com>
2026-08-06 11:45:27 +0530

    code

---
+++ 03_Datatypes.js
@@ -0,0 +1,28 @@
+//!PRIMITIVE DATATYPES
+
+//!1)NUMBER
+var num1 = 10;
+var num2 = 10.25;
+console.log(num1) //10
+console.log(typeof num1) //number
+console.log(num2) //10.25
+console.log(typeof num2) //number
+
+//!2)STRING
+let str1 = 'Yasin';
+let str2 = "Monty";
+let str3 = `Pavan`
+console.log(str1) //Yasin  
+console.log(typeof str1) //string
+console.log(str2) //Monty
+console.log(typeof str2) //string
+console.log(str3) //Pavan
+console.log(typeof str3) //string
+
+//!3) BOOLEAN
+const isLoggedIn = true;
+const feelingSleepy = false;
+console.log(isLoggedIn) //true
+console.log(typeof isLoggedIn) //boolean
+console.log(feelingSleepy) //false
+console.log(typeof feelingSleepy) //boolean
