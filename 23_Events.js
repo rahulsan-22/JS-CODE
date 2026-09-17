89dba88ec401cbf79b12170f79e424b7eff8c9ca
Rahul S <rahulsan098@gmail.com>
2026-09-17 11:56:08 +0530

    code

--- 23_Events.js
+++ 23_Events.js
@@ -23,25 +23,83 @@
 // })
 
 //!Types
-let nameField = document.getElementById("name")
-console.log(nameField)
+// let nameField = document.getElementById("name")
+// console.log(nameField)
 
 //!Input
-nameField.addEventListener("input", (e) => {
-  console.log(e.target.value)
-})
+// nameField.addEventListener("input", (e) => {
+//   console.log(e.target.value)
+// })
 
 //!Change
-nameField.addEventListener("change", (e) => {
-  e.target.style.border = "10px dashed red"
-  console.log(e.target.value + " 🤗")
-})
+// nameField.addEventListener("change", (e) => {
+//   e.target.style.border = "10px dashed red"
+//   console.log(e.target.value + " 🤗")
+// })
 
 //!Focus
-nameField.addEventListener("focus", (e) => {
-  e.target.style.background="aqua"
-})
+// nameField.addEventListener("focus", (e) => {
+//   e.target.style.background="aqua"
+// })
 //!Blur
-nameField.addEventListener("blur", (e) => {
-  e.target.style.background="lime"
+// nameField.addEventListener("blur", (e) => {
+//   e.target.style.background="lime"
+// })
+
+//!Keydown
+// let videoField = document.getElementById("videoField")
+// let video = document.querySelector("video")
+// console.log(videoField, video)
+// videoField.addEventListener("keydown", () => {
+//   console.log("Key is down")
+//   video.play()
+// })
+
+//!Keyup
+// videoField.addEventListener("keyup", () => {
+//   console.log("Key is up")
+//   video.pause()
+// })
+
+//!Domcontentloaded
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("Dom Tree Generated")
+// })
+
+//!Event Propagation
+// let outer = document.getElementById("outer")
+// let inner = document.getElementById("inner")
+
+//!Event Bubbling Phase
+// outer.addEventListener("click", () => {
+//   alert("Outer Box Clicked 😠")
+// },false)
+
+// inner.addEventListener("click", () => {
+//   alert("Inner Box Clicked 😉")
+// }, false)
+
+//!Event Capturing Phase
+// outer.addEventListener("click", () => {
+//   alert("Outer Box Clicked 😠")
+// },true)
+
+// inner.addEventListener("click", () => {
+//   alert("Inner Box Clicked 😉")
+// },true)
+
+//!stopPropagation()
+// outer.addEventListener("click", () => {
+//   alert("Outer Box Clicked 😠")
+// },false)
+
+// inner.addEventListener("click", (e) => {
+//   alert("Inner Box Clicked 😉")
+//   e.stopPropagation()
+// }, false)
+
+//!Event Delegation
+let ol = document.querySelector("ol")
+ol.addEventListener("click", (e) => {
+  console.log(e.target.textContent)
 })
