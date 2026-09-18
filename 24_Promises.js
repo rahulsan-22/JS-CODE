f8736cf43523e6067906f018c686d00fd0f5703e
Rahul S <rahulsan098@gmail.com>
2026-09-18 11:59:46 +0530

    code

---
+++ 24_Promises.js
@@ -0,0 +1,52 @@
+//!Promise
+// let p = new Promise((resolve, reject) => {
+//   let pistaHouse = true;
+//   if (pistaHouse) {
+//     resolve("Promise got resolved")
+//   } else {
+//     reject("Promise got rejected")
+//   }
+// })
+// console.log(p)
+// console.log(typeof p)
+// p.then((res) => {
+//   console.log(res + "Guys lets go inside n have something")
+// })
+// .catch((err) => {
+//     console.log(err + "Sorry Guys i had a meeting")
+// })
+// .finally(() => {
+//     console.log("Done")
+// })
+
+//!Promise Chaining
+// Promise.resolve(10)
+//   .then((res) => {
+//     return res * 2
+//   }).then((res) => {
+//     return res + 20
+//   }).then((res) => {
+//     console.log(res + 100)
+//   })
+
+//!Promise.resolve()
+// Promise.resolve("Hello")
+//   .then((res) => {
+//     console.log(res)
+//   })
+
+//!Promise.reject()
+// Promise.reject("Bye")
+//   .catch((err) => {
+//     console.log(err)
+//   })
+
+//!Promise.all()
+Promise.all([
+  Promise.resolve(10),
+  Promise.resolve(20),
+  Promise.resolve(30)
+])
+.then((res) => {
+    console.log(res)
+})
